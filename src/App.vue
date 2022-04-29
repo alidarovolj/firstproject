60e72d69533307a0f2366303941cb6745025ed43
Olzhas <oljasalidarov@gmail.com>
2022-04-29 21:54:35 +0600

    directives

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,18 @@
       <p @click="showAlert()" v-if="test === 1">Hello World!</p>
       <p @click="test = 1" v-if="test === 0">Hello Vue!</p>
 
-      </div>
+    </div>
 
-      <div v-for="item of items" :key="item">
-        <div class="block">
-          <p class="text-green-500">Hello!</p>
-          <p>{{ item }}</p>
-        </div>
+    <div v-for="item of items" :key="item">
+      <div class="block">
+        <p class="text-green-500">Hello!</p>
+        <p>{{ item }}</p>
       </div>
+    </div>
 
+    <input v-model="inpData" type="text" class="border border-black"><br>
+
+    <button @click="increment()">+</button><p>Counter: {{ counter }}</p><button @click="decrement()">-</button>
   </div>
 </template>
 
@@ -28,6 +31,8 @@ export default {
   name: "App",
   data() {
     return {
+      counter: 0,
+      inpData: null,
       test: 0,
       items: [0,1,2,3,4,5,6,7]
     }
@@ -35,6 +40,12 @@ export default {
   methods: {
     showAlert() {
       alert('hello World')
+    },
+    increment() {
+      this.counter = this.counter + 1
+    },
+    decrement() {
+      this.counter = this.counter - 1
     }
   }
 };
