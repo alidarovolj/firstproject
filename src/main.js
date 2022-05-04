e004aaed5c22ba502df148f9e953a236c105a691
Olzhas <oljasalidarov@gmail.com>
2022-05-04 19:56:11 +0600

    router + fonts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './assets/fonts.css'
 import './assets/main.less'
@@ -7,5 +8,6 @@ import './assets/main.less'
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
